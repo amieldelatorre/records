0e8de7e961081d0425bbb7a3ed0968e75d3f5acf
amieldelatorre <amieljames.delatorre@gmail.com>
2024-10-01 16:14:12 +1300

    test create user request values

--- src/backend/Records/UnitTests/Core/Applications/Features/UserFeatures/CreateUser/CreateUserValidatorTests.cs
+++ src/backend/Records/UnitTests/Core/Applications/Features/UserFeatures/CreateUser/CreateUserValidatorTests.cs
@@ -45,7 +45,61 @@ public class CreateUserValidatorTests
                     {"Password", ["The length of 'Password' must be at least 8 characters. You entered 6 characters."]},
                 }
             }
-        }
+        },
+        new object[]
+        {
+            new CreateUserRequest()
+            {
+                FirstName = "Valid",
+                LastName = "Valid",
+                Email = "mail@example.invalid",
+                Password = "not val"
+            },
+            new ExpectedValidationResult()
+            {
+                Result = false,
+                Errors = new Dictionary<string, string[]>
+                {
+                    {"Password", ["The length of 'Password' must be at least 8 characters. You entered 7 characters.", "The 'Password' must not contain whitespace."]},
+                }
+            }
+        },
+        new object[]
+        {
+            new CreateUserRequest()
+            {
+                FirstName = null,
+                LastName = null,
+                Email = null,
+                Password = null
+            },
+            new ExpectedValidationResult()
+            {
+                Result = false,
+                Errors = new Dictionary<string, string[]>
+                {
+                   {"FirstName", ["'First Name' must not be empty."]},
+                   {"LastName", ["'Last Name' must not be empty."]},
+                   {"Email", ["'Email' must not be empty."]},
+                   {"Password", ["'Password' must not be empty."]}
+                }
+            }
+        },
+        new object[]
+        {
+            new CreateUserRequest()
+            {
+                FirstName = "Albert",
+                LastName = "Einstein",
+                Email = "albert.einstein@example.invalid",
+                Password = "hispassword"
+            },
+            new ExpectedValidationResult()
+            {
+                Result = true,
+                Errors = new Dictionary<string, string[]>()
+            }
+        },
     ];
 
     // TODO: Add more test cases
@@ -54,11 +108,12 @@ public class CreateUserValidatorTests
     {
         var validator = new CreateUserValidator();
         var results = validator.Validate(request);
+        var resultsErrorDictionary = results.ToDictionary();
 
         Assert.Multiple(() =>
         {
             Assert.That(results.IsValid, Is.EqualTo(expected.Result));
-            Assert.That(results.ToDictionary(), Is.EqualTo(expected.Errors));
+            Assert.That(resultsErrorDictionary, Is.EqualTo(expected.Errors));
         });
     }
 }
