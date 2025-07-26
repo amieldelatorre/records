6cfc740cc6385c16a80bbcba5e4bec872686a438
amieldelatorre <amieljames.delatorre@gmail.com>
2025-07-27 11:28:58 +1200

    feat: add script to restore dev env from db dump

---
+++ scripts/restore_dev_env.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -eou pipefail
+
+SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")
+PROJECT_ROOT="${SCRIPT_ROOT}/.."
+# Go to project root
+cd "${PROJECT_ROOT}"
+
+docker compose down 
+sleep_time=20
+rm -rf ./.docker_data
+sleep 2
+
+docker compose up app-db -d
+echo "Sleeping for ${sleep_time}s to let containers start up successfully" 
+sleep "${sleep_time}"
+
+filepath="${PROJECT_ROOT}/src/backend/IntegrationTests/Data/app_postgres_dump.sql"
+cat "${filepath}"  | docker exec -i app-db psql -U records
